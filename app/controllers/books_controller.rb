713380dccaf2600311eb7198ccdc209449203434
Tajiiii <daisuke.tajima.0218@gmail.com>
2020-08-13 21:07:05 +0900

    [Add]if

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,14 +8,15 @@ class BooksController < ApplicationController
   	@book = Book.find(params[:id])
   end
 
-  def new
-  	@book = Book.new
-  end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id)
+  	@book = Book.new(book_params)
+  	if @book.save
+  		redirect_to book_path(@book.id)
+  	else
+  		@books = Book.all
+  		render 'index'
+  	end
   end
 
 
